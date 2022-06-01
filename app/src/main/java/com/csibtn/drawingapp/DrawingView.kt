8a19a259771914a2756357be96f22137c0fc88a1
CSIBTN <krutinivan3@gmail.com>
2022-06-01 12:47:03 +0300

    Added brush sizing

--- app/src/main/java/com/csibtn/drawingapp/DrawingView.kt
+++ app/src/main/java/com/csibtn/drawingapp/DrawingView.kt
@@ -3,6 +3,8 @@ package com.csibtn.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
 
 class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs) {
@@ -13,6 +15,7 @@ class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs
     private var brushSize : Float = 0f
     private var color = Color.BLACK
     private lateinit var canvas: Canvas
+    private val paths = mutableListOf<CustomPath>()
 
     init {
         setUp()
@@ -26,10 +29,9 @@ class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs
         drawingPaint.strokeJoin = Paint.Join.ROUND
         drawingPaint.strokeCap = Paint.Cap.ROUND
         canvasPaint = Paint(Paint.DITHER_FLAG)
-        brushSize = 20f
     }
 
-    internal inner class CustomPath(val color : Int, val brushSickness : Float) : Path(){
+    internal inner class CustomPath(var color : Int, var brushSickness : Float) : Path(){
 
     }
 
@@ -43,6 +45,48 @@ class DrawingView(context : Context, attrs : AttributeSet) : View(context, attrs
         super.onDraw(canvas)
         canvas.drawBitmap(canvasBitmap,0f,0f,canvasPaint)
 
-        canvas.drawPath(drawPath,drawingPaint)
+        for (path in paths){
+            drawingPaint.strokeWidth = path.brushSickness
+            drawingPaint.color = path.color
+            canvas.drawPath(path, drawingPaint)
+        }
+
+        if(!drawPath.isEmpty) {
+            drawingPaint.strokeWidth = drawPath.brushSickness
+            drawingPaint.color = drawPath.color
+            canvas.drawPath(drawPath, drawingPaint)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushSickness = brushSize
+
+                drawPath.reset()
+                drawPath.moveTo(touchX!!,touchY!!)
+            }
+            MotionEvent.ACTION_MOVE ->{
+                drawPath.lineTo(touchX!!,touchY!!)
+            }
+            MotionEvent.ACTION_UP -> {
+                paths.add(drawPath)
+                drawPath = CustomPath(color,brushSize)
+            }
+            else -> return false
+        }
+        invalidate()
+
+        return true
+    }
+
+    fun setSizeForBrush(newSize : Float){
+        brushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        newSize,
+        resources.displayMetrics)
+        drawingPaint.strokeWidth = brushSize
     }
 }
--- app/src/main/java/com/csibtn/drawingapp/MainActivity.kt
+++ app/src/main/java/com/csibtn/drawingapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.csibtn.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.csibtn.drawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawingView : DrawingView
+    private lateinit var binder : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+        val brushBtn = binder.ibBrushPick
+        drawingView = binder.drawingView
+
+        drawingView.setSizeForBrush(30f)
+
+
+        brushBtn.setOnClickListener{
+            showBrushSizeDialog()
+        }
+    }
+
+    private fun  showBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn = brushDialog.findViewById<ImageButton>(R.id.smallBrush)
+        val mediumBtn = brushDialog.findViewById<ImageButton>(R.id.mediumBrush)
+        val largeBtn = brushDialog.findViewById<ImageButton>(R.id.largeBrush)
+
+        smallBtn.setOnClickListener{
+            drawingView.setSizeForBrush(15f)
+            brushDialog.dismiss()
+        }
+
+        mediumBtn.setOnClickListener{
+            drawingView.setSizeForBrush(35f)
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener{
+            drawingView.setSizeForBrush(60f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
     }
 }
